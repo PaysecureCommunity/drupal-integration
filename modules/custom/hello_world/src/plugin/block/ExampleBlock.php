0cad6d7b53b8b4893e891096357eef3f28c4ff6d
Hardik Joshi <hardik2000jioshi@gmail.com>
2025-05-23 15:18:39 -0700

    Admin settings form and updated form configuration

--- modules/custom/hello_world/src/plugin/block/ExampleBlock.php
+++ modules/custom/hello_world/src/plugin/block/ExampleBlock.php
@@ -1,56 +1,67 @@
 <?php
     namespace Drupal\hello_world\Plugin\Block;
+
     use Drupal\Core\Block\BlockBase;
-    use Drupal\core\Form\FormStateInterface;
+    use Drupal\Core\Form\FormStateInterface;
+
      /**
       * php doc(documentation block)- used to document code in a way that tools like ide can understand
       * provides a 'Hello World' block
+      * 
     * @Block(
     * id="hello_world_block",
     * admin_label=@Translation("Hello World Block"),
     * )
     */
     class ExampleBlock extends BlockBase {
+
         /**
          * defaultConfiguration() sets default block message
+         * 
          * {@inheritdoc}
          */
         public function defaultConfiguration() {
+            // defaultConfiguration() method used to load initial values
+            $default_config = \Drupal::config('hello_world.settings'); // drupal stores file under namespace: hello_world.settings
             return [
-                'hello_message' => $this->t('Hello World!'),
+                'hello_message' => $default_config->get('hello.name'), // used to fetch config from module's hello_world.settings.yml
+                // get('hello.name') returns 'Hello world'
             ];
         }
 
         /**
          * inheritdoc part of PHPDoc documentation standard used across PHP projects
          * build() method- display the block content or message
+         * 
          * {@inheritdoc} 
          */
-    public function build(){
+        public function build(){
         return[
-            '#markup' => $this->configuration('hello_message'),
+            '#markup' => $this->configuration['hello_message'],
         ];
     }
 
     /**
+     * Adds a config form to the block settings in admin.
+     * 
      * {@inheritdoc} 
-     * blockForm() method allows us to create configuration form for custom block, creating configuration inteface in drupal admin UI
      */
     public function blockForm($form, FormStateInterface $form_state) {
         $form['hello_message']=[
             '#type' => 'textfield',
             '#title' => $this->t('Message'),
-            '#default_value' => $this->configuration('hello_message'),
-        ];
+            '#default_value' => $this->configuration['hello_message'],
+        ];  
         
         return $form;
     }
 
     /**
      * blockSubmit() method saves the form submission
+     * 
      * {@inheritdoc}
      */
     public function blockSubmit($form, FormStateInterface $form_state) {
         $this->configuration['hello_message']=$form_state->getValue('hello_message');
     }       
-}
+}   
